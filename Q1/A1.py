298b9b6c8fece9d68daae959f4e7178e3c495093
ramith123 <ramithchandula@gmail.com>
2020-06-11 18:41:32 -0400

    client side and dependency

--- Q1/A1.py
+++ Q1/A1.py
@@ -1,1 +1,29 @@
+from socket import socket, gethostname
+
+
+port = 12345
+
+
+def ClientsendMessage(message, sock):
+    # Send a server given message thorugh given socket
+    try:
+        print("Sending message...")
+        sock.sendall(message)
+        print("Message sent, Closing Connection.")
+        sock.close()
+    except:
+        print("Connection Error. Aborting")
+        exit()
+
+
+def connectToServer():
+    sock = socket()
+    try:
+        sock.connect((gethostname(), port))
+    except ConnectionRefusedError:
+        print("Server Unavailable")
+    print("Server Connection successfull.")
+    return sock
+
+
 print("Heelow world")
